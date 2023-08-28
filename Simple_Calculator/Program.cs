98c56b424467052a526e9b45967a99869520d85f
Ali Faal <faal.ali.faal@gmail.com>
2023-08-28 15:37:52 +0330

    Modified code

--- Simple_Calculator/Program.cs
+++ Simple_Calculator/Program.cs
@@ -8,81 +8,16 @@ namespace SimpleCalculator
         {
             Console.WriteLine("Simple Calculator");
 
-            double num1, num2;
-            bool isValidInput = false;
+            double num1 = GetValidNumber("Entere the first value");
 
-            do
-            {
-                Console.Write("Enter the first value: ");
-                if (double.TryParse(Console.ReadLine(), out num1))
-                {
-                    isValidInput = true;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Please enter a valid number.");
-                }
-            } while (!isValidInput);
+            double num2 = GetValidNumber("Entere the second value");
 
-            isValidInput = false;
+            ShowMenu();
 
-            do
-            {
-                Console.Write("Enter the second value: ");
-                if (double.TryParse(Console.ReadLine(), out num2))
-                {
-                    isValidInput = true;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Please enter a valid number.");
-                }
-            } while (!isValidInput);
+            int choice = GetValidChoice();
 
-            Console.WriteLine("Select an operation:");
-            Console.WriteLine("1. Add");
-            Console.WriteLine("2. Subtract");
-            Console.WriteLine("3. Multiply");
-            Console.WriteLine("4. Divide");
-            Console.WriteLine("5. Power");
 
-            int choice;
-            do
-            {
-                Console.Write("Enter your choice (1-5): ");
-                if (int.TryParse(Console.ReadLine(), out choice) && choice >= 1 && choice <= 5)
-                {
-                    isValidInput = true;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
-                }
-            } while (!isValidInput);
-
-            double result = 0;
-
-            switch (choice)
-            {
-                case 1:
-                    result = add(num1,num2);
-                    break;
-                case 2:
-                    result = sub(num1,num2);
-                    break;
-                case 3:
-                    result = mult(num1,num2);
-                    break;
-                case 4:
-                    result = div(num1,num2);
-                    break;
-                case 5:
-                    result = Math.Pow(num1, num2);
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice!");
-                    break;
-            }
+            double result = PerformOperation(choice, num1, num2);
 
             Console.WriteLine($"Result: {result}");
 
@@ -114,6 +49,77 @@ namespace SimpleCalculator
         private static double add(double num1, double num2)
         {
             return num1 + num2;
+        }
+
+        private static void ShowMenu()
+        {
+            Console.WriteLine("Select an operation:");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Subtract");
+            Console.WriteLine("3. Multiply");
+            Console.WriteLine("4. Divide");
+            Console.WriteLine("5. Power");
+        }
+
+        private static double PerformOperation(int choice, double num1, double num2)
+        {
+            switch (choice)
+            {
+                case 1:
+                    return add(num1, num2);
+                case 2:
+                    return sub(num1, num2);
+                case 3:
+                    return mult(num1, num2);
+                case 4:
+                    return div(num1, num2);
+                case 5:
+                    return Math.Pow(num1, num2);
+                default:
+                    Console.WriteLine("Invalid choice!");
+                    return 0;
+            }
+        }
+
+        static double GetValidNumber(string message)
+        {
+            double number;
+            bool isValidInput = false;
+
+            do
+            {
+                Console.WriteLine(message);
+                if (double.TryParse(Console.ReadLine(), out number))
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            } while (!isValidInput);
+
+            return number;
+        }
+
+        static int GetValidChoice()
+        {
+            int choice;
+            bool isValidInput = false;
+
+            do
+            {
+                Console.Write("Enter your choice (1-5): ");
+                if (int.TryParse(Console.ReadLine(), out choice) && choice >= 1 && choice <= 5)
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice.");
+                }
+            } while (!isValidInput);
+            return choice;
         }
     }
 }
